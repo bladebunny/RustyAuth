4a6e0556330a2943d7a642c24e103128f9d48737
Tim Brooks <tim.a.brooks@disney.com>
2024-05-26 21:37:57 -0700

    package update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RustyAuth",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v12), .iOS(.v16), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
